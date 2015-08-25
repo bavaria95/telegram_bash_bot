1a342a2158423ec2371f3f87856c33214f0b53ba
Dmitry Petruk <bavaria95@gmail.com>
2015-08-25 18:36:18 +0300

    rewrote parser. now it's parsing also id of the quote and makes key-value pairs od id-quote

--- parse.rb
+++ parse.rb
@@ -5,4 +5,12 @@ require 'nokogiri'
 
 url = 'http://bash.im/best'
 
-quotes = Nokogiri::HTML(open(url)).css("div[class='text']").to_a.map{|q| q.children.to_a}
+quotes = {}
+
+Nokogiri::HTML(open(url)).css("div[class='quote']").to_a.map{ |x| 
+	quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").children.to_a
+}
+
+p quotes
+
+
