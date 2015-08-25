0846c86faea19dcad8e4ed5d4f39225148e700f9
Dmitry Petruk <bavaria95@gmail.com>
2015-08-25 20:13:05 +0300

    replacing <br> tag with newline

--- parse.rb
+++ parse.rb
@@ -8,11 +8,7 @@ url = 'http://bash.im/best'
 quotes = {}
 
 Nokogiri::HTML(open(url)).css("div[class='quote']").to_a.map{ |x| 
-	quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").children.to_a
+	quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").
+			children.to_a.map{|s| s.name == 'br' ? '\n' : s.text}
 }
-
 quotes.delete nil
-
-p quotes
-
-
