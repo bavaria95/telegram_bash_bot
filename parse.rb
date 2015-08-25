28414745f811a154e7f46d5e4a3d15d29d904de9
Dmitry Petruk <bavaria95@gmail.com>
2015-08-25 20:15:44 +0300

    removed converting to array(map works on enumerator)

--- parse.rb
+++ parse.rb
@@ -7,8 +7,8 @@ url = 'http://bash.im/best'
 
 quotes = {}
 
-Nokogiri::HTML(open(url)).css("div[class='quote']").to_a.map{ |x| 
+Nokogiri::HTML(open(url)).css("div[class='quote']").map{ |x| 
 	quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").
-			children.to_a.map{|s| s.name == 'br' ? '\n' : s.text}
+			children.map{|s| s.name == 'br' ? '\n' : s.text}
 }
 quotes.delete nil
