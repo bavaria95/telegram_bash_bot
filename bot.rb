bb9fc764c271288ae40dfa67294de71301109c4e
Dmitry Petruk <bavaria95@gmail.com>
2015-08-25 22:10:20 +0300

    bot, which undestands /random command and sends one random quote from bash.im

---
+++ bot.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+
+require 'open-uri'
+require 'nokogiri'
+require 'telegram/bot'
+
+def get_random_quote
+
+	url = 'http://bash.im/random'
+
+	quotes = {}
+
+	Nokogiri::HTML(open(url)).css("div[class='quote']").map{ |x| 
+		quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").
+				children.map{|s| s.name == 'br' ? '\n' : s.text}.join
+	}
+	quotes.delete nil
+
+	quotes[quotes.keys.sample]
+end
+
+token = File.read('token.dat')
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    
+    when '/random'
+      bot.api.sendMessage(chat_id: message.chat.id, text: get_random_quote)
+    
+	else
+		bot.api.sendMessage(chat_id: message.chat.id, text: "Unrecognized command")
+    end
+  end
+end
