6ae5530607cf90dfaf68fca8d2a6e3687176d5bb
Dmitry Petruk <bavaria95@gmail.com>
2015-08-25 17:38:08 +0300

    getting only text(with inner tags)

--- parse.rb
+++ parse.rb
@@ -5,4 +5,4 @@ require 'nokogiri'
 
 url = 'http://bash.im/best'
 
-quotes = Nokogiri::HTML(open(url)).css("div[class='text']").to_a
+quotes = Nokogiri::HTML(open(url)).css("div[class='text']").to_a.map{|q| q.children.to_a}
