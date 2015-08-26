004bcfd184aa18391659e5bfe90e0803f494f3cf
Dmitry Petruk <bavaria95@gmail.com>
2015-08-26 22:57:54 +0300

    cleaned up

--- bot.rb
+++ bot.rb
@@ -4,21 +4,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'telegram/bot'
 
-def get_random_quote
-
-	url = 'http://bash.im/random'
-
-	quotes = {}
-
-	Nokogiri::HTML(open(url)).css("div[class='quote']").map{ |x| 
-		quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").
-				children.map{|s| s.name == 'br' ? "\n" : s.text}.join
-	}
-	quotes.delete nil
-
-	quotes[quotes.keys.sample]
-end
-
 token = File.read('token.dat')
 
 Telegram::Bot::Client.run(token) do |bot|
