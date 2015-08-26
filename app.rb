7c90fd1eb05a325fdcb77b9f970d62b0dc1f533d
Dmitry Petruk <bavaria95@gmail.com>
2015-08-26 22:55:03 +0300

    returns random quote or today's quotes

--- app.rb
+++ app.rb
@@ -5,24 +5,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-def get_random_quote
-
-	url = 'http://bash.im/random'
-
-	quotes = {}
-
-	Nokogiri::HTML(open(url)).css("div[class='quote']").map{ |x| 
-		quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").
-				children.map{|s| s.name == 'br' ? "\n" : s.text}.join
-	}
-	quotes.delete nil
-
-	quotes[quotes.keys.sample]
-end
-
-def get_todays_quotes
-
-	url = 'http://bash.im/best'
+def parse_quotes url
 
 	quotes = {}
 
@@ -35,7 +18,11 @@ def get_todays_quotes
 	quotes
 end
 
-
 get '/random' do 
-	get_todays_quotes.to_json
+	quotes = parse_quotes("http://bash.im/random")
+	quotes[quotes.keys.sample].to_json
+end
+
+get '/today' do
+	parse_quotes("http://bash.im/best").to_json
 end
