22bbfe324987a5252d888b204290f1bf360fb45e
Dmitry Petruk <bavaria95@gmail.com>
2015-08-26 22:47:05 +0300

    scaffold for sinatra app

--- app.rb
+++ app.rb
@@ -19,7 +19,22 @@ def get_random_quote
 	quotes[quotes.keys.sample]
 end
 
+def get_todays_quotes
+
+	url = 'http://bash.im/best'
+
+	quotes = {}
+
+	Nokogiri::HTML(open(url)).css("div[class='quote']").map{ |x| 
+		quotes[x.css("div.actions a.id").children.text[1..-1]] = x.css("div[class='text']").
+				children.map{|s| s.name == 'br' ? "\n" : s.text}.join
+	}
+	quotes.delete nil
+
+	# quotes
+end
+
 
 get '/random' do 
-	get_random_quote
+	get_todays_quotes
 end
