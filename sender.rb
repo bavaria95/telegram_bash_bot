58b509194ef6ea3a37a709f21b8df2f19c2a7b83
Dmitry Petruk <bavaria95@gmail.com>
2015-08-28 00:35:45 +0300

    added configuration

--- sender.rb
+++ sender.rb
@@ -4,3 +4,10 @@ require 'open-uri'
 require 'telegram/bot'
 require 'oj'
 require 'redis'
+
+token = File.read('token.dat')
+host = "pub-redis-16230.us-east-1-4.6.ec2.redislabs.com"
+port = 16230
+redis_pass = File.read('redis_pass.dat')
+
+url_random = "http://127.0.0.1:4567/best"
