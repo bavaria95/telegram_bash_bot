73ec15cf96584a58524314bf29b1f3c269a4c306
Dmitry Petruk <bavaria95@gmail.com>
2015-08-27 15:29:53 +0300

    custom keyboards for all cases

--- bot.rb
+++ bot.rb
@@ -13,35 +13,53 @@ redis_pass = File.read('redis_pass.dat')
 
 redis = Redis.new(:host => host, :port => port, :password => redis_pass)
 
+
+def custom_keyboard(redis, id)
+	if redis.sismember('users', id)
+			hints = Telegram::Bot::Types::ReplyKeyboardMarkup
+				.new(keyboard: [%w(Random)])
+	else
+		hints = Telegram::Bot::Types::ReplyKeyboardMarkup
+		.new(keyboard: [%w(Subscribe), %w(Random)])
+	end
+
+	hints
+end
+
+
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
 
     when '/start'
-    	hints = Telegram::Bot::Types::ReplyKeyboardMarkup
-      		.new(keyboard: [%w(Subscribe), %w(Random)])
       	bot.api.sendMessage(chat_id: message.chat.id, text: "Welcome to our Bash bot!\n" +
-      		"You can subscribe here on daily digest of quotes or request random quote.", reply_markup: hints)
+      		"You can subscribe here on daily digest of quotes or request random quote.", 
+      		reply_markup: custom_keyboard(redis, message.chat.id))
     
     when 'Random'
-      bot.api.sendMessage(chat_id: message.chat.id, text: Oj.load(open("http://127.0.0.1:4567/random").read))
+      	bot.api.sendMessage(chat_id: message.chat.id, reply_markup: custom_keyboard(redis, message.chat.id), 
+      		text: Oj.load(open("http://127.0.0.1:4567/random").read))
     
-    when 'Subscribe'
-    	chat_id = message.chat.id
-    	if redis.sismember('users', chat_id)
-    		bot.api.sendMessage(chat_id: chat_id, text: "You are already subscribed.")
-    	else
-    		if redis.sadd('users', chat_id)
-    			bot.api.sendMessage(chat_id: chat_id, text: "You have been successfully subscribed.")
-    			File.open('users.dat', 'a+') { |file| file.write("OK: ADD #{chat_id}\n") }
-    		else
-    			bot.api.sendMessage(chat_id: chat_id, text: "Sorry, but something went wrong on our end.")
-    			File.open('users.dat', 'a+') { |file| file.write("ERROR: ADD #{chat_id}\n") }
-    		end
-    	end
+	when 'Subscribe'
+		chat_id = message.chat.id
+		if redis.sismember('users', chat_id)
+			bot.api.sendMessage(chat_id: chat_id, text: "You are already subscribed.",
+			 reply_markup: custom_keyboard(redis, message.chat.id))
+		else
+			if redis.sadd('users', chat_id)
+				bot.api.sendMessage(chat_id: chat_id, text: "You have been successfully subscribed.", 
+					reply_markup: custom_keyboard(redis, message.chat.id))
+				File.open('users.dat', 'a+') { |file| file.write("OK: ADD #{chat_id}\n") }
+			else
+				bot.api.sendMessage(chat_id: chat_id, text: "Sorry, but something went wrong on our end.",
+					reply_markup: custom_keyboard(redis, message.chat.id))
+				File.open('users.dat', 'a+') { |file| file.write("ERROR: ADD #{chat_id}\n") }
+			end
+		end
 
 	else
-		bot.api.sendMessage(chat_id: message.chat.id, text: "Unrecognized command")
+		bot.api.sendMessage(chat_id: message.chat.id, text: "Unrecognized command", 
+			reply_markup: custom_keyboard(redis, message.chat.id))
     end
   end
 end
